842dd1439dfcb230c03526e0f5c3ebfb36d832c5
abenrejeb <ext-abenrejeb@leparisien.fr>
2023-10-27 16:40:38 +0200

    Add isActive in menu

--- client/src/components/LeftNav.js
+++ client/src/components/LeftNav.js
@@ -6,15 +6,15 @@ const LeftNav = () => {
         <div className="left-nav-container">
             <div className="icons">
                 <div className="icons-bis">
-                    <NavLink to='/' exact="true" className="active-left-nav">
+                    <NavLink to='/' end exact="true" className={({isActive}) => isActive ? "active-left-nav" : ""}>
                         <img src="./img/icons/home.svg" alt="home"/>
                     </NavLink>
                     <br/>
-                    <NavLink to='/trending' exact="true">
+                    <NavLink to='/trending' exact="true" className={({isActive}) => isActive ? "active-left-nav" : ""}>
                         <img src="./img/icons/rocket.svg" alt="home"/>
                     </NavLink>
                     <br/>
-                    <NavLink to='/profil' exact="true">
+                    <NavLink to='/profil' exact="true" className={({isActive}) => isActive ? "active-left-nav" : ""}>
                         <img src="./img/icons/user.svg" alt="home"/>
                     </NavLink>
                 </div>
